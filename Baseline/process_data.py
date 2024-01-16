6ede15f16423efe36dcb198e6c209abb6f6e92a1
rf2bNdb <134395785+rf2bNdb@users.noreply.github.com>
2024-01-16 16:45:17 +0800

    Update process_data.py

--- Baseline/process_data.py
+++ Baseline/process_data.py
@@ -3,19 +3,25 @@ import pandas as pd
 import scanpy as sc
 import numpy as np
 
+import anndata as ad
 def add_celltype(adata:ad.AnnData, group:list, meta:pd.DataFrame):
     '''
-    处理Haber等人的数据<< A single-cell survey of the small intestinal epithelium>>
+    处理Haber等人的数据。
+    这样使用：group = ['B1','B2','B3','B4','B9','B10']
+            adata_1 = add_celltype(adata, group, meta)
+
     '''
     cell_indeies_list = []
+    celltype_meta =pd.DataFrame()
     num = 0
     
     for i in range(len(group)):
         meta_group = meta[meta['group']==group[i]]
         meta_group_list = meta_group['cell.name'].tolist()
         cell_group = adata[adata.obs['group']==group[i]].obs.index.str.split('-')
-        
-        
+        celltype_meta_group = meta['cell.type'][meta['group']==group[i]]
+        celltype_meta = pd.concat([celltype_meta, celltype_meta_group],axis=0)
+
         meta_index_list=[]
         cell_index_list=[]
         for j in range(len(cell_group)):
@@ -40,5 +46,7 @@ def add_celltype(adata:ad.AnnData, group:list, meta:pd.DataFrame):
 
     #把结果拼一起
     adata = adata[cell_indeies_list,:]
+    celltype_meta.index = adata.obs.index
+    adata.obs['cell.type']=celltype_meta
     
     return adata
